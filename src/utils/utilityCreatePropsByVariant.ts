89bcbd760aa8cdc02606ff8d91ed7fd3b5cc1893
Roberto Valenza <r.valenza@bitrocket.dev>
2023-04-07 18:13:20 +0200

    feat: added avatar component from rocket lib to storybook

---
+++ src/utils/utilityCreatePropsByVariant.ts
@@ -0,0 +1,51 @@
+/** @format */
+
+import { TFontTag } from '../components-ui/Text/partials/Formatter';
+
+interface TextProps {
+  size: string;
+  weight: string;
+  tag: TFontTag;
+  transform?: string;
+}
+
+export const utilityCreatePropsByVariant = (variant: string): TextProps => {
+  switch (variant) {
+    case 'display':
+      return {
+        tag: 'h1',
+        weight: 'black',
+        size: 'display',
+      };
+    case 'header':
+      return {
+        tag: 'h2',
+        weight: 'black',
+        size: 'header',
+      };
+    case 'title':
+      return {
+        tag: 'h3',
+        weight: 'medium',
+        size: 'title',
+      };
+    case 'subtitle':
+      return {
+        tag: 'h4',
+        weight: 'bold',
+        size: 'subtitle',
+      };
+    case 'caption':
+      return {
+        tag: 'p',
+        weight: 'regular',
+        size: 'caption',
+      };
+    default:
+      return {
+        tag: 'span',
+        weight: 'regular',
+        size: 'caption',
+      };
+  }
+};
