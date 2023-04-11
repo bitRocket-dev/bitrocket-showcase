cee5a3d61a788b9339367d51025780a8a8a86caf
Roberto Valenza <112887605+robertovalenza-bitrocketdev@users.noreply.github.com>
2023-04-11 15:37:26 +0200

    feat: added radio butto from rocket lib (#17)

---
+++ src/stories/Radio.stories.tsx
@@ -0,0 +1,28 @@
+/** @format */
+
+import { ChangeEvent, useState } from 'react';
+
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Props, UIRadioButton } from '../components-ui/Radio';
+
+export default {
+  title: 'Example/RadioButton',
+  component: UIRadioButton,
+  argTypes: {
+    onChange: { table: { disable: true } },
+    checked: { table: { disable: true } },
+  },
+} as Meta;
+
+const Template: Story<Props> = props => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => setIsChecked(event.target.checked);
+
+  return <UIRadioButton {...props} checked={isChecked} onChange={onChange} />;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {};
