4b10449ce0747f6d89319e78f1e21ec4481d8469
Roberto Valenza <112887605+robertovalenza-bitrocketdev@users.noreply.github.com>
2023-04-13 15:52:35 +0200

    Aggiunto ButtonCTA Cosmocode su storybook (#30)
    * fix: fixed error build on vercel
    
    * feat: added button cta from cosmocode to storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@
 
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Props, UIButton } from '../components-ui/Button';
+import { Props, UIButton } from '../components-ui/Button/Button';
 
 export default {
   title: 'Example/Button',
